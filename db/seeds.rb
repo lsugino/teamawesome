d70edfb982cd9ed520fdf48dd80bc87d14757ecb
Apprentice <apprentice@dbc25.local>
2013-10-25 16:31:29 -0700

    Seeds contain tests.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,25 @@
 # require_relative '../config/application'
 require 'faker'
 
+# Task.delete_all
+# List.delete_all
+
+p vanessas_list = List.create(:name => "v_superpowers")
+p nicoles_list = List.create(:name => "n_superpowers")
+
+p vanessas_list.tasks.create(:task => "learn to fly")
+p nicoles_list.tasks.create(:task => "learn to be invisible")
 
 
-Task.delete_all
+5.times do
+  vanessas_list.tasks.create(:task => Faker::Lorem.sentence)
+end
 
+5.times do
+  nicoles_list.tasks.create(:task => Faker::Lorem.sentence)
+end
 
-  vanessas_list = List.create(:name => Faker::Lorem.words.join)
-  p vanessas_list.id
-  learning = vanessas_list.tasks.create( :task => "learn to fly" )
-  p learning.list_id
-  p learning.id
-
-
-
-
+# p vanessas_list.id == 16
+# p nicoles_list.id == 17
 
 
