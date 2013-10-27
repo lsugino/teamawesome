a1dada4b53f701d2de8b0e43a48882fffc2c5c51
Apprentice <apprentice@dbc25.local>
2013-10-26 21:12:45 -0700

    changed syntax in task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,21 @@
-# require_relative '../../config/application'
-
 
 class Task < ActiveRecord::Base
   belongs_to :lists
+
+  def self.add(string)
+    added = create(:task => string)
+    added.save
+  end
+
+  def self.delete(to_delete)
+    deleted = find(to_delete.first.to_i).destroy
+    deleted.save
+  end
+
+  def self.complete(completed)
+    comp = find(completed.first.to_i)
+    comp.update_attribute(:completed?, true)
+    comp.save
+  end
 
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
-describe Task do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# describe Task do
+#   # pending "add some examples to (or delete) #{__FILE__}"
+#   before(:all) do
+#     List.delete_all
+#     Task.delete_all
+#     @list = List.new #create(:name => "Vanessa's List")
+#     @task = Task.new #create(:task => "Learn to fly")#, :list_id => @list.id)
+#     @list.assign_attributes(
+#       name: "Vanessa's List"
+#     )
+#     @task.assign_attributes(
+#       task: "Learn to fly",
+#       list_id: @list.id
+#     )
+#   end
+
+#   it "should have a foreign key" do
+#     @list.id == @task.list_id
+#   end
+# end
